94c3458f04d714778c994374bdb5c9f558a896ad
debussysuh <debussysuh@gmail.com>
2023-08-21 12:10:37 +0900

    feat: 시큐리티와 jwt를 사용한 회원가입/로그인 구현

--- src/main/java/com/example/vblogserver/domain/user/entity/User.java
+++ src/main/java/com/example/vblogserver/domain/user/entity/User.java
@@ -7,58 +7,50 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "users")
 @Getter
-@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "USERS")
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "USER_ID")
+    @Column(name = "user_id")
     private Long id;
-    //private String password;
-
-    //private String username; // 이름
-    @Column(unique = true, nullable = false)
-    private String email; // 가입 이메일
-
-    @Column(nullable = false, unique = true, length = 15)  // 닉네임 중복 허용 할건지?
-    private String nickname; // 닉네임
-    //private String profileUrl; // 프로필 사진
+    private String email; // 이메일
+    private String loginid; // 아이디
+    private String password; // 비밀번호
+    private String username; // 이름
+    private String imageUrl; // 프로필 이미지
     @CreatedDate
     private LocalDateTime createDate; // 가입 날짜
 
-    //OAuth2
-    private String provider; // google, naver, kakao
-    private String providerId; // OAuth의 key(id)
-
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private Role role;
 
-    @Builder
-    public User(String email, String nickname, String provider, String providerId, Role role) {
-        this.email = email;
-        this.nickname = nickname;
-        this.provider = provider;
-        this.providerId = providerId;
-        this.role = role;
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType; // KAKAO, NAVER, GOOGLE
+    private String socialId; // 로그인한 소셜 타입의 식별자 값 (일반 로그인인 경우 null)
+
+    private String refreshToken; // 리프레시 토큰
+
+    // 유저 권한 설정 메소드
+    public void authorizeUser() {
+        this.role = Role.USER;
     }
 
-    public String getRoleKey() {
-        return this.role.getKey();
+    // 비밀번호 암호화 메소드
+    public void passwordEncode(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
     }
 
-    public User updateNickname(String nickname) {
-        this.nickname = nickname;
-        return this;
+    public void updateRefreshToken(String updateRefreshToken) {
+        this.refreshToken = updateRefreshToken;
     }
-
-    //public void setPassword(String password) { this.password = password; }
 
 }
--- src/main/java/com/example/vblogserver/global/config/WebMvcConfig.java
+++ src/main/java/com/example/vblogserver/global/config/WebMvcConfig.java
@@ -12,7 +12,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+            .allowedOrigins("*")
+            .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
