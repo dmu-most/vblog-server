cd802bd68e0da3600172c1aef0755fe9451e9de5
debussysuh <debussysuh@gmail.com>
2023-09-12 01:38:07 +0900

    chore: TokenController 엔드포인트 수정

--- src/main/java/com/example/vblogserver/global/jwt/controller/TokenController.java
+++ src/main/java/com/example/vblogserver/global/jwt/controller/TokenController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.vblogserver.domain.user.dto.ResponseDto;
@@ -20,6 +21,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping("/token")
 @RequiredArgsConstructor
 public class TokenController {
 
@@ -27,7 +29,7 @@ public class TokenController {
 	private final UserRepository userRepository;
 
 	// 액세스 토큰 검증
-	@GetMapping("/verify-access-token")
+	@GetMapping("/verify-access")
 	public ResponseEntity<ResponseDto> verifyAccessToken(HttpServletRequest request) {
 		Optional<String> accessTokenOpt = jwtService.extractAccessToken(request);
 
@@ -46,7 +48,7 @@ public class TokenController {
 	}
 
 	// 리프레시 토큰으로 액세스 토큰 재발급
-	@PostMapping("/refresh-access-token")
+	@PostMapping("/refresh-access")
 	public ResponseEntity<ResponseDto> refreshAccessToken(HttpServletRequest request, HttpServletResponse response) {
 		Optional<String> refreshTokenOpt = jwtService.extractRefreshToken(request);
 
