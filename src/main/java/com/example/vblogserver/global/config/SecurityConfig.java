bbd23bd69612947e0305e911c92c2f2a5ab39d3d
debussysuh <debussysuh@gmail.com>
2023-09-18 14:28:43 +0900

    chore: 응답 헤더 설정 추가

--- src/main/java/com/example/vblogserver/global/config/SecurityConfig.java
+++ src/main/java/com/example/vblogserver/global/config/SecurityConfig.java
@@ -164,10 +164,10 @@ public class SecurityConfig {
 			Arrays.asList("http://localhost:3000", "http://dmu-vblog.s3-website.ap-northeast-2.amazonaws.com"));
 		configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		configuration.setAllowCredentials(true);
-		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Refresh", "Cache-Control", "Content-Type"));
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Refresh", "Cache-Control", "Content-Type", "Location"));
 
 		/* 응답 헤더 설정 추가*/
-		configuration.setExposedHeaders(Arrays.asList("Authorization", "Refresh"));
+		configuration.setExposedHeaders(Arrays.asList("Authorization", "Refresh", "Location"));
 
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
